4b334128c4b1bf7a404d7adbce893724baa36203
vsoch <vsoch@users.noreply.github.com>
2025-03-16 07:23:43 -0600

    experiment: state machine flux cpu iteration 2
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

---
+++ experiments/aws-march-2025/state-machine-flux/results/cpu/iter-2/structure_054823014/createsim/entrypoint.sh
@@ -0,0 +1,43 @@
+
+  
+  jobid="structure_054823014"
+  outpath="/mnt/efs/iter-1/structure_054823014/createsim"
+  
+
+  echo ">> jobid        = $jobid"
+  echo ">> outpath      = $outpath"
+  
+  echo ">> hostname     = "$(hostname)
+  mkdir -p -v $outpath; cd $outpath
+
+  
+
+  MUMMI_APP=/opt/clones/mummi-ras
+  MUMMI_RESOURCES=/opt/clones/mummi_resources
+  export MUMMI_APP MUMMI_RESOURCES
+  export MUMMI_ROOT=$(dirname $(dirname $outpath))
+  locpath=$(dirname $outpath)
+  echo "Outpath is $outpath, Local path is $locpath" 
+  rm -rf $locpath/tmp
+  mkdir -p $locpath/tmp
+  cd ${locpath}
+  export OMP_NUM_THREADS=$(nproc)
+  outfile=$(ls $locpath/mlrunner/structure*.gro)
+  echo "MLrunner output file is $outfile"
+  cp ${outfile} $locpath/tmp/${jobid}.gro
+  cd $locpath/tmp
+  mummi_createsim \
+    --fstype simple \
+    --patch ${jobid} \
+    --inpath $(pwd) \
+    --outpath $locpath/createsim \
+    --outlocal $locpath/tmp \
+    --logpath $locpath \
+    --loglevel 2 \
+    --gromacs gmx \
+    --mpi "gmx mdrun" \
+    --mini \
+    --mdrunopt " -ntmpi 1 -ntomp $OMP_NUM_THREADS -pin off"
+
+
+  
